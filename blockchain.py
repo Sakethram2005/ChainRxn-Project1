092b6f9080d469203f9cba8b397b262815c56bd2
Thammandra Saketh Ram <thammandrasr@iitbhilai.ac.in>
2025-06-10 15:44:23 +0530

    Initial blockchain project

---
+++ blockchain.py
@@ -0,0 +1,48 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        last_block = self.get_last_block()
+        new_block = Block(
+            index=last_block.index + 1,
+            timestamp=time.time(),
+            data=data,
+            previous_hash=last_block.hash
+        )
+        self.chain.append(new_block)
+
+    def print_chain(self):
+        for block in self.chain:
+            print(f"Block #{block.index}")
+            print(f"Timestamp: {block.timestamp}")
+            print(f"Data: {block.data}")
+            print(f"Hash: {block.hash}")
+            print(f"Previous Hash: {block.previous_hash}")
+            print("-" * 40)
+
+my_blockchain = Blockchain()
+my_blockchain.add_block("First Block")
+my_blockchain.add_block("Second Block")
+my_blockchain.print_chain()
